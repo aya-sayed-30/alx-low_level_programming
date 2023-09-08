18b218bfe61e5d7c041cad80ea7b430c24a07588
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-08 16:28:07 -0700

    ;:

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,16 +14,16 @@ int main(void)
     srand(time(0));
 
     /* Generate a random number */
-    n = rand() - RAND_MAX / 2;
+n = rand() - RAND_MAX / 2;
 
     /* Check if the number is positive, negative, or zero */
-    if (n > 0) 
-        printf("%d is positive\n", n);
-    else if (n < 0) 
-        printf("%d is negative\n", n);
-    else
-        printf("%d is zero\n", n);
-    return (0);
+if (n > 0) 
+    printf("%d is positive\n", n);
+else if (n < 0) 
+    printf("%d is negative\n", n);
+else
+    printf("%d is zero\n", n);
+return (0);
 }
 
 

dd927fe59d3183df08bd55abd3957d4adae3149b
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-09 08:31:04 -0700

    last-digit-task

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,8 +2,6 @@
 #include <time.h>
 #include <stdio.h>
 #include <math.h>
-/* more headers goes there */
-
 /**
 *  main - checked n if is less than 6 and not 0 or greater than 5 or is 0
 *
@@ -14,10 +12,6 @@ int main(void)
 int n, last_Digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
-/* Cela retournera le nombre total de chiffres - 1 */
-/* l = log10(n); */
-/* pre = n / pow(10, l); */
 last_Digit = n % 10;
 if (last_Digit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, last_Digit);

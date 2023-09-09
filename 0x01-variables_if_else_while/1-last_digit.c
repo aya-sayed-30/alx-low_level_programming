62c3f207a12a74dbe97d8427233d98442a53c913
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-09 08:14:00 -0700

    last_digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,23 +1,29 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include <stdlib.h>
-/** 
- * main - entry point
- * Return: Always 0 (Success)
- */
-int main(void) 
+#include <stdio.h>
+#include <math.h>
+/* more headers goes there */
+
+/**
+*  main - checked n if is less than 6 and not 0 or greater than 5 or is 0
+*
+* Return: int
+*/
+int main(void)
 {
 int n, lastDigit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* extract the last digit of n */
+/* your code goes there */
+/* Cela retournera le nombre total de chiffres - 1 */
+/* l = log10(n); */
+/* pre = n / pow(10, l); */
 lastDigit = n % 10;
 if (lastDigit > 5)
 printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit);
-else if (lastDigit == 0)
-printf("Last digit of %d is %d and is "0\n", n, lastDigit);
-else
+else if (der == 0)
+printf("Last digit of %d is %d and is 0\n", n, lastDigit);
+else if (der < 6 && n != 0)
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit);
 return (0);
 }

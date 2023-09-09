446cbd6fa0fc132e35f57196ba2f9af42e452302
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-09 10:13:12 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,11 +3,10 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-
-int main(void) 
+int main(void)
 {
     int i;
-    for (i = 0; i <= 9; i++) 
+    for (i = 0; i <= 9; i++)
     {
         printf("%d", i);
     }

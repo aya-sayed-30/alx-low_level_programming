f0ccca519af6df49e802de738a41e790a89af117
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-11 10:27:20 -0700

    popo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-
 /**
- * * main - Print combinations of two digit numbers
- * *
- * * Return: Always 0 (Success)
+ * main - Print combinations of two digit numbers
+ * Return: Always 0 (Success)
  */
 int main(void)
 {

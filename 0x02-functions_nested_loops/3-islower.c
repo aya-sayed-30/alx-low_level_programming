a447a593f18ec4165c9bb53654412e28a2425f20
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-12 16:24:44 -0700

    slower-3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,17 +2,13 @@
 
 /**
  * Return: 1 if true and 0 if false
- *
  */
-int _islower(int c);
+
+int _islower(int c)
 {
-	if (c >=97 && c <=122)
-	{
-		return(1);
-	}
+	if ((c >= 'a') && (c <= 'z'))
+		return (1);
+
 	else
-	{
-		return(0);
-	}
-	_putchar('\n);
+		return (0);
 }

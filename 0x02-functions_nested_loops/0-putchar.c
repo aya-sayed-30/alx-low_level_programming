008f1a2df744aaf87704d190db15127c6a58ba4f
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-12 15:07:31 -0700

    commit the change

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
+#include "main.h"
+
 /**
  * main - Entry point
  * Return: Always 0 (Success)
  * Task - Prints "_putchar" followed by new line
  * Auth - ALABI TIMOTHY
  */
-
-#include "main.h"
 
 int main(void)
 {

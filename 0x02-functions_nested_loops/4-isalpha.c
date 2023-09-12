b51a2c7582b7fea6f6a6b5294cefa0e65f79fb67
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-12 16:31:55 -0700

    commit on this task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if an input
+ * @c: character is lower or not
+ * Return: 1 if true and 0 if false
+ */
+
+int _isalpha(int c)
+{
+	if (isalpha(c))
+		return (1);
+	else
+		return (0);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-2-islower
+isalpha

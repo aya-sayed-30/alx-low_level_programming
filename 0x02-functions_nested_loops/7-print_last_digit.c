77cb09f3a1b1fdb1bd8d7090081246b4b52164ee
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-12 17:02:55 -0700

    commit for printing last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @n: the parameter value
+ * Return: returns the last digit value
+ */
+
+int print_last_digit(int n)
+{
+      int ld;
+      ld = n % 10;
+      if (ld < 0)
+      {
+	 ld = -(ld);
+	_putchar(ld + '0');
+	return (ld);
+      }
+
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,1 +1,1 @@
-6-abs
+7-print last digit

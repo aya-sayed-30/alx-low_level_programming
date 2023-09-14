649a30cfc8b966264b8e479c752700a7225abf4d
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-14 13:52:53 -0700

    commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
 *print_numbers- a function that prints the numbers, from 0 to 9
 *
@@ -12,5 +13,3 @@ void print_numbers(void)
 		_putchar(i + '0');
 	_putchar('\n');
 }
-
-ubuntu:/debugging$

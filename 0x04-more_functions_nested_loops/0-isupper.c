7776f4a6be768c812f7f48ad647d6ff39f4aa399
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-14 12:15:04 -0700

    commit on the task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
-* int _isupper - returns 1 if uppercase characters and 0 otherwise
+* _isupper - returns 1 if uppercase alphabet and 0 otherwise
 * @c: takes in a character
 * Return: 0 for uppercase, 1 for lowercase
 **/

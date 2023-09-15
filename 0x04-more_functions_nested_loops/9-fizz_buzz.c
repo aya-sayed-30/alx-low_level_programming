05b4ff2e622c5f3a3026814843cb55d7560eb37a
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-14 18:52:57 -0700

    fizz-buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - entry block
+ * Return: 0 Success
+ *
+ */
+
+int main(void)
+{
+	int c;
+
+	c = 1;
+	printf("%d", c);
+	for (c = 2; c <= 100; c++)
+	{
+		if ((c % 3 == 0) && (c % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (c % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (c % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", c);
+		}
+	}
+	printf("\n");
+	return (0);
+}

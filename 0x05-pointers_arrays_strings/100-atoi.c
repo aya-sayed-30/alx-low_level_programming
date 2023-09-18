351c25e9aa1772828f7c605ef4089dcb6ad78ea9
aya sayed <aya.sayed.ahmad@gmail.com>
2023-09-18 14:49:38 -0700

    commit on task-100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,29 +10,29 @@
 
 int _atoi(char *s)
 {
-	int n, k, q;
+	int n, i, m;
 
-	q = 1;
-	k = n = 0;
-	while ((s[k] < '0' || s[k] > '9') && s[k] != 0)
+	m = 1;
+	i = n = 0;
+	while ((s[i] < '0' || s[i] > '9') && s[i] != 0)
 	{
-		if ([k] == '-')
-			q = q * -1;
-		k++;
+		if (s[i] == '-')
+			m = m * -1;
+		i++;
 	}
-	while ((s[k] >= '0' && s[k] <= '9') && s[k] != 0)
+	while ((s[i] >= '0' && s[i] <= '9') && s[i] != 0)
 	{
 		if (n >= 0)
 		{
-			n = n * 10 - (s[k] - '0');
-			k++;
+			n = n * 10 - (s[i] - '0');
+			i++;
 		}
 		else
 		{
-			n = n * 10 - (s[k] - '0');
-			k++;
+			n = n * 10 - (s[i] - '0');
+			i++;
 		}
 	}
-	q = q * -1;
-	return (n * q);
+	m = m * -1;
+	return (n * m);
 }

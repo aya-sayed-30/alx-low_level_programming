bd650958f65465c27d8a28d1000ce7765ff558ca
aya-sayed-30 <aya.sayed.ahmad@gmail.com>
2023-10-04 13:07:27 -0700

    commit on the code

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,23 +4,24 @@
  * create_array - creates an array of chars.
  * @size: size of the array.
  * @c: storaged char
+ *
  * Return: pointer of an array of chars
  */
 char *create_array(unsigned int size, char c)
 {
-	char *ch;
+	char *cr;
 	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	ch = malloc(sizeof(c) * size);
+	cr = malloc(sizeof(c) * size);
 
-	if (ch == NULL)
+	if (cr == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		ch[i] = c;
+		cr[i] = c;
 
-		return (ch);
+	return (cr);
 }
